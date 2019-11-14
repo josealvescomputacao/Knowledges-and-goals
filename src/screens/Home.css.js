2a9827f0262ba0ea6df4c8df35d47c2ad929e0a1
jose alves junior <josealvesufmt@gmail.com>
2019-11-13 20:26:36 -0400

    Update Home.css.js

--- src/screens/Home.css.js
+++ src/screens/Home.css.js
@@ -1,45 +1,54 @@
 import styled from 'styled-components'
 
 export const Footer = styled.div`
-    @media (max-width: 320px)  {             
+
+    @media (min-width: 280px) and (max-width: 552px){ 
+        
         span{
             font-size: 16px
         }
-    }
+        bottom: 20px
+        left: 30%
+        transform: translate(-20%, 0)
 
-    @media (min-width: 320px) and (max-width: 552px){ 
-        
-        span{
-            font-size: 18px
-        }
     }
 
     @media (min-width: 552px) and (max-width: 768px){ 
         
         span{
-            font-size: 20px
+            font-size: 18px
         }
+        bottom: 100px
+        left: 50%
+        transform: translate(-50%, 0)
     }
 
     @media (min-width: 768px) and (max-width: 992px){     
         span{
             font-size: 22px
         }
+        bottom: 80px
+        left: 50%
+        transform: translate(-50%, 0)
     }
     @media (min-width: 992px){ 
         span{
             font-size: 24px
         }
+        bottom: 35px
+        left: 50%
+        transform: translate(-50%, 0)
+
     }
 
 `
 
 export const carousel = {
     width: '75vw', 
-    overflowX: 'hidden',
+    maxHeight:'85vh',
     borderRadius:'10px', 
     margin:'20px auto auto auto', 
-    boxShadow:'10px 10px 20px 10px rgba(0,0,0,0.99)'
+    boxShadow:'10px 10px 20px 10px rgba(0,0,0,0.99)',
 }
 
 export const titleImage = {
